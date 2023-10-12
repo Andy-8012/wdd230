8e760280cd060406615013d9784ccba05093dd01
Andy-8012 <andrewvriley@gmail.com>
2023-10-12 14:14:07 -0600

    updated links to json

---
+++ scripts/links.js
@@ -0,0 +1,32 @@
+const baseURL = "https://Andy-8012.github.io/wdd230/";
+//const linksURL = "https://Andy-8012.github.io/wdd230/data/links.json";
+const linksURL = "data/links.json"
+const cardList = document.querySelector('.cardList');
+
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    displayLinks(data.weeks);
+}
+
+getLinks();
+
+function displayLinks(weeks){
+    weeks.forEach(object => {
+        let liElement = document.createElement('li');
+        liElement.textContent = `${object.week}: `;
+
+        object.links.forEach(object => {
+            let aElement = document.createElement('a');
+            aElement.setAttribute("href",object.url);
+            aElement.textContent = ` ${object.title} |`;
+
+            console.log(aElement);
+
+            liElement.appendChild(aElement);
+        });
+
+        cardList.appendChild(liElement);
+    });
+}
+
