a6dda8e828582c08f033879ce93b06db36e17880
Andy-8012 <andrewvriley@gmail.com>
2023-10-24 21:51:04 -0600

    Fixed menu links add actual banner picture

--- chamber/scripts/menuButton.js
+++ chamber/scripts/menuButton.js
@@ -5,3 +5,9 @@ hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
 });
+
+const joinButton = document.querySelector('#joinButton')
+
+joinButton.addEventListener('click', () => {
+	window.location.href="join.html"
+})
